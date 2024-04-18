f9bbc7bfec7e0fcd5dae1b1d3d4c2addfe5d29a0
HaikalDev27 <72543199+HaikalDev27@users.noreply.github.com>
2024-04-18 21:14:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollLinks = document.querySelectorAll('a[href^="#"]');
+
+    for (var i = 0; i < scrollLinks.length; i++) {
+        scrollLinks[i].addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute("href");
+            var targetElement = document.querySelector(targetId);
+
+            targetElement.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        });
+    }
+});
